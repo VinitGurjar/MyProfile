413862c7b8b4501824308831f2d1ca3b65f069ff
adhiramaa <vinitfagna@gmail.com>
2023-07-20 23:56:17 +0530

    Add: auth provider

--- app/auth-form.tsx
+++ app/auth-form.tsx
@@ -14,7 +14,7 @@ export default function AuthForm() {
       appearance={{ theme: ThemeSupa }}
       theme="dark"
       showLinks={false}
-      providers={[]}
+      providers={["github"]}
       redirectTo="http://localhost:3000/auth/callback"
     />
   );
